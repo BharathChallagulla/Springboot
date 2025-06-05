47096902e71cdd1b092940a9f803974133c4708f
Bharath Challagulla <chlgbk2000@gmail.com>
2025-06-05 11:58:02 -0500

    using @RequestParam annotation for getting request params

--- SpringBootWeb1/src/main/java/com/bharath/SpringBootWeb1/HomeController.java
+++ SpringBootWeb1/src/main/java/com/bharath/SpringBootWeb1/HomeController.java
@@ -5,6 +5,7 @@ import jakarta.servlet.http.HttpSession;
 import org.apache.catalina.Session;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
@@ -16,10 +17,12 @@ public class HomeController {
     }
 
     @RequestMapping("/add")
-    public String addNumbers(HttpServletRequest req, HttpSession session){
+//    public String addNumbers(HttpServletRequest req, HttpSession session){
+//    using request param
+    public String addNumbers(@RequestParam("num1") int num1, @RequestParam("num2") int num2, HttpSession session){
 
-        int num1 = Integer.parseInt(req.getParameter("num1"));
-        int num2 = Integer.parseInt(req.getParameter("num2"));
+//        int num1 = Integer.parseInt(req.getParameter("num1"));
+//        int num2 = Integer.parseInt(req.getParameter("num2"));
         int result = num1+num2;
 
         session.setAttribute("result", result);
