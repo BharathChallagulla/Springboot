7fc3a49b357a47bbb35d08bf779b16ff122fb0c9
Bharath Challagulla <chlgbk2000@gmail.com>
2025-06-13 20:02:24 -0500

    updated project name

--- JobApp/src/main/java/com/example/JobApp/controller/JobController.java
+++ JobApp/src/main/java/com/example/JobApp/controller/JobController.java
@@ -2,12 +2,20 @@ package com.example.JobApp.controller;
 
 
 import com.example.JobApp.model.JobPost;
+import com.example.JobApp.service.JobService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Controller
 public class JobController {
+
+    @Autowired
+    JobService jobService;
 
     @GetMapping(value = {"/", "home"})
     public String home(){
@@ -21,11 +29,14 @@ public class JobController {
 
     @PostMapping("handleForm")
     public String handleAddJob(JobPost jobPost){
+        jobService.addJob(jobPost);
         return "success";
     }
 
     @GetMapping("viewalljobs")
-    public String viewAllJobs(){
-        return "veiwalljobs";
+    public String viewAllJobs(Model m) {
+        List<JobPost> jobs = jobService.getAllJobs();
+        m.addAttribute("jobPosts", jobs);
+        return "viewalljobs";
     }
 }
