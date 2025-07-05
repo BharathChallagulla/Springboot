7fca60a3aed191ab7cc71189eabb648f4ce75ef2
Bharath Challagulla <chlgbk2000@gmail.com>
2025-07-05 17:37:25 -0500

    springboot starter for ecom code

---
+++ EcommerceWeb/src/test/java/com/bharath/EcommerceWeb/EcommerceWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bharath.EcommerceWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
